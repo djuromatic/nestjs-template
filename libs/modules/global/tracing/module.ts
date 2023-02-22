cdd26bbeb2f382d9b247035393c9309f60d439bb
Djuro Matic <djuromatic991@gmail.com>
2023-02-22 21:40:42 +0100

    removing commented code

--- libs/modules/global/tracing/module.ts
+++ libs/modules/global/tracing/module.ts
@@ -3,20 +3,6 @@ import { SecretsModule } from '../secrets/module';
 
 import { TracingService } from './open-telemetry';
 
-// const envConfig = () => {
-//   const option: ConfigModuleOptions = {
-//     isGlobal: true,
-//     load: [appConfiguration],
-//     cache: true, // env variables will be cached in the memory.
-//   };
-
-//   if (process.env.NODE_ENV === 'local') {
-//     option.envFilePath = [`.env.local`];
-//   }
-
-//   return option;
-// };
-
 @Module({
   imports: [SecretsModule],
   providers: [TracingService],
