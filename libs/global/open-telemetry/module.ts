4043745aaf0a337fe8c4324663e709380d3d974a
Djuro Matic <djuromatic991@gmail.com>
2023-02-23 16:54:38 +0100

    removing unused stuff

--- libs/global/open-telemetry/module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OpenTelemetryService } from './service';
-
-@Module({
-  imports: [],
-  providers: [OpenTelemetryService],
-  exports: [OpenTelemetryService],
-})
-export class OpenTelemetryModule {}
